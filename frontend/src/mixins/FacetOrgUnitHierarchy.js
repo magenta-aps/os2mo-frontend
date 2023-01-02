88c58ce6f3ec920c903d4ddab8fcec28e182327d
Thor Astrup Pedersen <tap@magenta-aps.dk>
2023-01-02 09:19:31 +0100

    [#49554] Add OrgUnit org_unit_hierarchy form fields org-unit modals

---
+++ frontend/src/mixins/FacetOrgUnitHierarchy.js
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2019-2020 Magenta ApS
+// SPDX-License-Identifier: MPL-2.0
+
+import { Facet } from "@/store/actions/facet"
+export default {
+  computed: {
+    facetOrgUnitHierarchy() {
+      let facet = this.$store.getters[Facet.getters.GET_FACET]("org_unit_hierarchy")
+      let result = [{ value: null, text: this.$t("shared.entire_organisation") }]
+      if ("classes" in facet) {
+        for (var cl of facet.classes) {
+          result.push({ value: cl.uuid, text: cl.name })
+        }
+      }
+      return result
+    },
+  },
+}
